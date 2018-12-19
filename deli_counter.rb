923e20a8ef4aeb2441fcd1cdceb285c459c08ea7
Simon Kong <simon.kong95@gmail.com>
2018-12-19 07:28:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,25 +2,25 @@
 
 def take_a_number(line, name)
   line.push(name);
-  return "Welcome, #{name}. You are number #{line.length} in line."
+  puts "Welcome, #{name}. You are number #{line.length} in line."
 end
 
 def now_serving(num)
   if num.length == 0
-    return "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
-    return "Currently serving " + num.shift() +"."
+    puts "Currently serving " + num.shift() +"."
   end
 end
 
 def line(name)
   i, j = "The line is currently:", 1
   if name.length == 0
-    return "The line is currently empty."
+    return puts "The line is currently empty."
   end
   name.each do |list|
     i << " #{j}. #{list}"
     j += 1
   end
-  i
+  puts i
 end
