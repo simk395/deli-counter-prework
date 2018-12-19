9d625e9f80947805cde5fad6f538da03caa29ad9
Simon Kong <simon.kong95@gmail.com>
2018-12-19 07:09:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def takeANumber(line, name)
+  line.push(name);
+  return "Welcome, "+ name +". You are number " + line.length +" in line.";
+end
+
+def nowServing(line)
+  if(line.length === 0)
+    return "There is nobody waiting to be served!";
+  else
+    return "Currently serving " + line.shift() +".";
+  end
+end
+
+def currentLine(line)
+  if(line.length === 0)
+    return "The line is currently empty."
+  else
+    i = 0;
+    while(i < line.length)
+      line[i] = " " + (i + 1) + ". " + line[i];
+      i++;
+    return "The line is currently:" + line;
+  end
+  return "Welcome, " + name + ". You are number "+ (line.length + 1)  + " in line.";
+end
+
